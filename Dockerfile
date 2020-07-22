afa5dd1dd3c97a4ad961ac11e429f30127455b74
cat <bamboo.cutecat@gmail.com>
2020-07-22 17:01:58 +0800

    cross-correlation & 全捲積化

--- Dockerfile
+++ Dockerfile
@@ -30,11 +30,34 @@ SHELL [ "/bin/bash", "--login", "-c" ]
 RUN source activate workenv\
 && conda install -y pylint\
 && conda install -y tensorflow\
-&& conda install -y rope
+&& conda install -y rope\
+&& conda install -y autopep8
 
 RUN apt-get update -y
-RUN apt-get install git-all -y
+RUN apt-get install git -y
 
 ADD siam /siam/
 WORKDIR /siam
 
+
+## env set up
+################################################################
+
+## nvidia gpu docker setting
+# distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+# curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+# curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+# sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
+# sudo systemctl restart docker
+# docker run --gpus all nvidia/cuda:10.0-base nvidia-smi
+
+##docker environment setting
+# sudo groupadd docker
+# sudo usermod -aG docker $USER
+
+## GUI settings
+# xhost +local:root
+
+## docker run cmd settings
+# docker run --gpus all -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY --device=/dev/video0:/dev/video0 -it visual_tracking /bin/bash
+################################################################
--- siam/main.py
+++ siam/main.py
@@ -1,5 +1,7 @@
 # owner Jules 曾筠筑
 import tensorflow as tf
+import data_loader
+import numpy as np
 
 
 def siamese(left_input_shape, right_input_shape):
@@ -21,22 +23,44 @@ def siamese(left_input_shape, right_input_shape):
                                      kernel_initializer=tf.keras.initializers.RandomNormal, kernel_regularizer=tf.keras.regularizers.L1L2))
 
     # TODO  修改為全捲積層
-    model.add(tf.keras.layers.Flatten())
-    model.add(tf.keras.layers.Dense(4096, activation='sigmoid', kernel_initializer=tf.keras.initializers.RandomNormal,
-                                    kernel_regularizer=tf.keras.regularizers.L1L2, bias_initializer=tf.keras.initializers.RandomNormal))
+    # model.add(tf.keras.layers.Flatten())
+    # model.add(tf.keras.layers.Dense(4096, activation='sigmoid', kernel_initializer=tf.keras.initializers.RandomNormal,
+    #                                 kernel_regularizer=tf.keras.regularizers.L1L2, bias_initializer=tf.keras.initializers.RandomNormal))
 
     feature_vector_left = model(left_input)
     feature_vector_right = model(right_input)
 
+    # 200719
+    feature_vector_left_filter = tf.reshape(feature_vector_left, shape=(
+        feature_vector_left.shape[1], feature_vector_left.shape[2], 256, 1))
+    heatmap = tf.keras.backend.conv2d(
+        feature_vector_right, feature_vector_left_filter,  strides=(1, 1), padding='same', data_format='channels_last', dilation_rate=(1, 1))
+
+    
+    # 200719
+
     # TODO  比對方式修改成捲積比對
-    L1_distance_layer = tf.keras.layers.Lambda(
-        lambda lefttensor, righttensor: tf.keras.backend.abs(lefttensor - righttensor))
+    # L1_distance_layer = tf.keras.layers.Lambda(
+    #     lambda tensor: tf.keras.backend.abs(tensor[0] - tensor[1]))
+    # L1_distance = L1_distance_layer(
+    #     [feature_vector_left, feature_vector_right])
+    # predictions = tf.keras.layers.Dense(
+    #     1, activation='sigmoid', bias_initializer=tf.keras.initializers.RandomNormal)(L1_distance)
 
-    L1_distance = L1_distance_layer(feature_vector_left, feature_vector_right)
+    resized_heatmap = tf.keras.layers.experimental.preprocessing.Resizing(
+        right_input.shape[1], right_input.shape[2])(heatmap)
 
-    predictions = tf.keras.layers.Dense(
-        1, activation='sigmoid', bias_initializer=tf.keras.initializers.RandomNormal)(L1_distance)
-    siamese_network=tf.keras.Model(input=[left_input,right_input],outputs=predictions)
+    siamese_network = tf.keras.Model(
+        inputs=[left_input, right_input], outputs=resized_heatmap)
 
     # TODO 輸出應為heatmap
     return siamese_network
+
+
+model = siamese((105, 105, 1), (200, 200, 1))
+model.summary()
+
+# optimizer = tf.keras.optimizers.Adagrad(lr=0.001)
+# model.compile(loss='categorical_crossentropy', optimizer=optimizer)
+
+# model.train_on_batch
